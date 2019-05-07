ef3b10501ac8a859bd84610456dc4c0044fdef38
Vinodh Ramamoorthy <vinodh.rp@gmail.com>
2019-05-07 15:02:21 +0530

    save

--- src/main/java/com/cloud/spring/user/controller/UserDetailController.java
+++ src/main/java/com/cloud/spring/user/controller/UserDetailController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +19,11 @@ import org.springframework.web.util.UriComponentsBuilder;
 import com.cloud.spring.user.entity.UserDetail;
 import com.cloud.spring.user.service.UserDetailService;
 import com.cloud.spring.user.util.CustomErrorType;
+import com.cloud.spring.user.util.SuccessMessage;
 
 @RestController
 @RequestMapping("user-service")
+@CrossOrigin
 public class UserDetailController {
 
 	public static Logger logger = LoggerFactory.getLogger(UserDetailController.class);
@@ -85,7 +88,8 @@ public class UserDetailController {
 
 		HttpHeaders headers = new HttpHeaders();
 		headers.setLocation(ucBuilder.path("/user-service/fetchuser/{id}").buildAndExpand(user.getUserId()).toUri());
-		return new ResponseEntity<String>("User Successfully Created/Updated : " +headers.getLocation(),headers, HttpStatus.CREATED);
+		return new ResponseEntity<SuccessMessage>(new SuccessMessage("User with id " + user.getUserId() + " Created"),
+				HttpStatus.CREATED);
 	}
 
 }
---
+++ src/main/java/com/cloud/spring/user/util/SuccessMessage.java
@@ -0,0 +1,19 @@
+package com.cloud.spring.user.util;
+
+public class SuccessMessage {
+	
+	public String successMsg;
+	
+	public SuccessMessage(String successMsg) {
+		this.successMsg = successMsg;
+	}
+
+	public String getSuccessMsg() {
+		return successMsg;
+	}
+
+	public void setSuccessMsg(String successMsg) {
+		this.successMsg = successMsg;
+	}
+
+}

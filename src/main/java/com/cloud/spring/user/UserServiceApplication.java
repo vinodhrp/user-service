a6d2b5e4c607c4c69232a4d89c9e2fece70c48ce
Vinodh Ramamoorthy <vinodh.rp@gmail.com>
2019-06-03 21:01:17 +0530

    ee

--- src/main/java/com/cloud/spring/user/UserServiceApplication.java
+++ src/main/java/com/cloud/spring/user/UserServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class UserServiceApplication {
 	
 	public static Logger logger = LoggerFactory.getLogger(UserServiceApplication.class);

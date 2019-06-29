3a4dace9cf446a7f421b19cc35c73152521c07f3
Vinodh Ramamoorthy <vinodh.rp@gmail.com>
2019-06-29 11:44:14 +0530

    config updates

--- src/main/java/com/cloud/spring/user/UserServiceApplication.java
+++ src/main/java/com/cloud/spring/user/UserServiceApplication.java
@@ -4,9 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class UserServiceApplication {
 	
 	public static Logger logger = LoggerFactory.getLogger(UserServiceApplication.class);

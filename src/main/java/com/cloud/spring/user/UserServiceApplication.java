c467fd2c1e30a8f776089f5872fbc8ae63cee3fa
Vinodh Ramamoorthy <vinodh.rp@gmail.com>
2019-07-01 22:52:40 +0530

    Update UserServiceApplication.java

--- src/main/java/com/cloud/spring/user/UserServiceApplication.java
+++ src/main/java/com/cloud/spring/user/UserServiceApplication.java
@@ -5,7 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @SpringBootApplication
 @EnableEurekaClient
 public class UserServiceApplication {
